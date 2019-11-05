730c1404e3dfdc4acbce992413ffcef5dfb0c15f
Miraclo <597738979@qq.com>
2019-11-05 14:13:48 +0000

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
+
+    //用户注册页面
     public function create()
     {
         return view('users.create');
     }
+
+
+    //用户页面
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+
+
 }
