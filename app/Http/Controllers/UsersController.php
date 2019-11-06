f15dd09cdc312d0e48aeab19cb095b86692931e9
Miraclo <597738979@qq.com>
2019-11-06 13:09:47 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -87,7 +87,10 @@ class UsersController extends Controller
     //个人中心页面
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+        return view('users.show', compact('user', 'statuses'));
     }
 
 
