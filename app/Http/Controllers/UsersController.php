8a333b98e7ade297640b1a12d69398dac56abe3e
Miraclo <597738979@qq.com>
2019-11-06 01:58:31 +0000

    更改用户资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,14 +37,41 @@ class UsersController extends Controller
     }
 
 
-
-
     //个人中心页面
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
 
+
+    //编辑页面
+    public function edit(User $user)
+    {
+
+        return view('users.edit', compact('user'));
+
+    }
+
+
+    //提交编辑数据
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user);
+    }
 
 
 
